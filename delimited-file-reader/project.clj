cd235122ae2105706516a4607d76708d89910dc7
Mike Hall <mike@just3ws.com>
2017-01-23 13:22:30 -0600

    clj-time is actually a dependency

--- delimited-file-reader/project.clj
+++ delimited-file-reader/project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/just3ws/coding-exercise-001"
   :license {:name "GNU General Public License version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-time "0.13.0"]]
   :main ^{:skip-aot true} delimited-file-reader.core
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
                              [venantius/ultra "0.5.1"]
-                             [cider/cider-nrepl "0.14.0"]
-                             [clj-time "0.13.0"]]}})
+                             [cider/cider-nrepl "0.14.0"]]}})
