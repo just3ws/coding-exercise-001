ccedc0ec64f7ca93c3c50fbc124e69f6a20414fd
Mike Hall <mike@just3ws.com>
2017-01-23 13:14:14 -0600

    Restored the main entrypoint

--- delimited-file-reader/project.clj
+++ delimited-file-reader/project.clj
@@ -4,7 +4,7 @@
   :license {:name "GNU General Public License version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main delimited-file-reader.core
+  :main ^{:skip-aot true} delimited-file-reader.core
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
                              [venantius/ultra "0.5.1"]
                              [cider/cider-nrepl "0.14.0"]]}})
--- delimited-file-reader/src/delimited_file_reader/core.clj
+++ delimited-file-reader/src/delimited_file_reader/core.clj
@@ -1,4 +1,10 @@
 (ns delimited-file-reader.core
   (:require [delimited-file-reader.records])
-  (:import [delimited_file_reader.records Person]))
+  (:import [delimited_file_reader.records Person])
+  (:gen-class))
+
+(defn -main
+  "Command-line entry point."
+  [path]
+  (println path))
 
