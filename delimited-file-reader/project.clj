0bf4f9087816795368d32661dcd104b684efbea3
Mike Hall <mike@just3ws.com>
2017-01-23 11:10:55 -0600

    Specs now running
    Added helpers for autotest and prettier test output

--- delimited-file-reader/project.clj
+++ delimited-file-reader/project.clj
@@ -4,4 +4,6 @@
   :license {:name "GNU General Public License version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main delimited-file-reader.core)
+  :main delimited-file-reader.core
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
+                             [venantius/ultra "0.5.1"]]}})
--- delimited-file-reader/test/delimited_file_reader/core_test.clj
+++ delimited-file-reader/test/delimited_file_reader/core_test.clj
@@ -3,5 +3,5 @@
             [delimited-file-reader.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Math still works."
+    (is (= 1 1))))
