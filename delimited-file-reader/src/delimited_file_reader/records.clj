321cb2a9b32b19318954662d76aa6d9ba57cbe45
Mike Hall <mike@just3ws.com>
2017-01-24 12:54:13 -0600

    Function to generate Person defrecord using ordinal and keyword arguments

--- delimited-file-reader/src/delimited_file_reader/records.clj
+++ delimited-file-reader/src/delimited_file_reader/records.clj
@@ -1,7 +1,12 @@
 (ns delimited-file-reader.records)
 
-(defrecord Person [last_name
-                   first_name
-                   gender
-                   date_of_birth
-                   favorite_color])
+(def fields (vector :last_name :first_name :gender :date_of_birth :favorite_color))
+
+(defrecord Person [last_name first_name gender date_of_birth favorite_color])
+
+(defn make-person
+  "Make a new Person record"
+  ([last_name first_name gender date_of_birth favorite_color]
+   (->Person last_name first_name gender date_of_birth favorite_color))
+  ([{:keys [last_name first_name gender date_of_birth favorite_color]}]
+   (make-person last_name first_name gender date_of_birth favorite_color)))
--- delimited-file-reader/test/delimited_file_reader/records_test.clj
+++ delimited-file-reader/test/delimited_file_reader/records_test.clj
@@ -3,13 +3,53 @@
             [delimited-file-reader.records :refer :all])
   (:import [delimited_file_reader.records Person]))
 
-(def valid-person (Person. "Hall" "Mike" "M" "12/19/1975" "Blue"))
+(def valid-last_name "Hall")
+(def valid-first_name "Mike")
+(def valid-gender"M")
+(def valid-date_of_birth "12/19/1975")
+(def valid-favorite_color "blue")
 
+;; Construct a raw defrecord
+(def valid-person (Person. valid-last_name
+                           valid-first_name
+                           valid-gender
+                           valid-date_of_birth
+                           valid-favorite_color))
 (deftest valid-person-test
-  (testing "A Person can be populated with valid data"
-    (is (= (get valid-person :last_name) "Hall"))
-    (is (= (get valid-person :first_name) "Mike"))
-    (is (= (get valid-person :gender) "M"))
-    (is (= (get valid-person :date_of_birth) "12/19/1975"))
-    (is (= (get valid-person :favorite_color) "Blue"))))
+  (testing "New Person record can be directly constructed"
+    (is (= (get valid-person :last_name) valid-last_name))
+    (is (= (get valid-person :first_name) valid-first_name))
+    (is (= (get valid-person :gender) valid-gender))
+    (is (= (get valid-person :date_of_birth) valid-date_of_birth))
+    (is (= (get valid-person :favorite_color) valid-favorite_color))))
 
+;; Construct with ordinal arguments
+(def make-valid-person-with-ordinal-arguments
+  (make-person valid-last_name
+               valid-first_name
+               valid-gender
+               valid-date_of_birth
+               valid-favorite_color))
+(deftest construct-person-with-ordinal-arguments-test
+  (testing "A Person can be constructed with valid ordinal arguments"
+    (is (= (get make-valid-person-with-ordinal-arguments :last_name) valid-last_name))
+    (is (= (get make-valid-person-with-ordinal-arguments :first_name) valid-first_name))
+    (is (= (get make-valid-person-with-ordinal-arguments :gender) valid-gender))
+    (is (= (get make-valid-person-with-ordinal-arguments :date_of_birth) valid-date_of_birth))
+    (is (= (get make-valid-person-with-ordinal-arguments :favorite_color) valid-favorite_color))))
+
+;; Construct with keyword arguments
+(def make-valid-person-with-keyword-arguments
+  (make-person {
+                :favorite_color valid-favorite_color
+                :date_of_birth valid-date_of_birth
+                :gender valid-gender
+                :first_name valid-first_name
+                :last_name valid-last_name }))
+(deftest construct-person-with-keyword-arguments-test
+  (testing "A Person can be constructed with valid keyword arguments"
+    (is (= (get make-valid-person-with-keyword-arguments :last_name) valid-last_name))
+    (is (= (get make-valid-person-with-keyword-arguments :first_name) valid-first_name))
+    (is (= (get make-valid-person-with-keyword-arguments :gender) valid-gender))
+    (is (= (get make-valid-person-with-keyword-arguments :date_of_birth) valid-date_of_birth))
+    (is (= (get make-valid-person-with-keyword-arguments :favorite_color) valid-favorite_color))))
