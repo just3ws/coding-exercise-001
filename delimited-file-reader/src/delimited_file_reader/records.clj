ec0ede1dec5524bf577a95da6327d2477ef8032c
Mike Hall <mike@just3ws.com>
2017-01-24 13:15:43 -0600

    Reduced some repetion by reading the defrecord documentation. :D

--- delimited-file-reader/src/delimited_file_reader/records.clj
+++ delimited-file-reader/src/delimited_file_reader/records.clj
@@ -13,5 +13,7 @@
   "Make a new Person record"
   ([last_name first_name gender date_of_birth favorite_color]
    (->Person last_name first_name gender date_of_birth favorite_color))
-  ([{ :keys [last_name first_name gender date_of_birth favorite_color] }]
-   (make-person last_name first_name gender date_of_birth favorite_color)))
+  ([{ :keys [last_name first_name gender date_of_birth favorite_color] :as fields }]
+   (map->Person fields)))
+
+
