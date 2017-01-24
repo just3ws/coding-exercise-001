c0e6da84e3c16776e2c30e671083b7087295b67b
Mike Hall <mike@just3ws.com>
2017-01-24 13:05:09 -0600

    Applied small formatting preference for curly braces

--- delimited-file-reader/project.clj
+++ delimited-file-reader/project.clj
@@ -1,11 +1,14 @@
 (defproject delimited-file-reader "0.1.0-SNAPSHOT"
   :description "Consume files that can either be pipe, comma, or space delimited."
   :url "http://github.com/just3ws/coding-exercise-001"
-  :license {:name "GNU General Public License version 3"
-            :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
+  :license {
+            :name "GNU General Public License version 3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.txt" }
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.13.0"]]
-  :main ^{:skip-aot true} delimited-file-reader.core
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
+  :main ^{ :skip-aot true } delimited-file-reader.core
+  :profiles {
+             :dev {
+                   :plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
                              [venantius/ultra "0.5.1"]
-                             [cider/cider-nrepl "0.14.0"]]}})
+                             [cider/cider-nrepl "0.14.0"]] } })
