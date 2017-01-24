b3701ffea6514e3c74cb02d495a1be4a03e8db8b
Mike Hall <mike@just3ws.com>
2017-01-24 14:03:55 -0600

    Added CodeClimate, TravisCI, and some plugins

--- delimited-file-reader/project.clj
+++ delimited-file-reader/project.clj
@@ -6,9 +6,14 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.txt" }
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.13.0"]]
-  :main ^{ :skip-aot true } delimited-file-reader.core
-  :profiles {
-             :dev {
-                   :plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
-                             [venantius/ultra "0.5.1"]
-                             [cider/cider-nrepl "0.14.0"]] } })
+  ;: main ^{ :skip-aot true } delimited-file-reader.core
+  :main delimited-file-reader.core
+  :aot [delimited-file-reader.core]
+  :plugins [[cider/cider-nrepl "0.14.0"] ;; Recommended for use with vim-fireplace
+            [com.jakemccrary/lein-test-refresh "0.18.1"] ;; Run tests on change
+            [lein-kibit "0.1.3"] ;; Clojure static analysis
+            [lein-ancient "0.6.10"] ;; Check for out-of-date dependencies
+            [jonase/eastwood "0.2.3"] ;; Clojure linting
+            [venantius/ultra "0.5.1"] ;; Improved test output, repl printing
+            [lein-pprint "1.1.2"]] ;; Pretty printing
+  :test-refresh { :notify-command ["terminal-notifier" "-title" "Tests" "-message"] })
