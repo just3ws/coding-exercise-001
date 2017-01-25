7f393be358d475faaf95206c94b17226a867a028
Mike Hall <mike@just3ws.com>
2017-01-24 23:42:45 -0600

    Added sniffing of lines to figure out type of file

--- src/delimited_file_reader/sniffer.clj
+++ src/delimited_file_reader/sniffer.clj
@@ -1,5 +1,24 @@
-(ns delimited-file-reader.sniffer)
+(ns delimited-file-reader.sniffer
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :refer :all]))
 
-(defn pipe-delimited? [str] true)
-(defn comma-delimited? [str] true)
-(defn space-delimited? [str] true)
+
+(defn open-file [path] (io/file (io/resource path)))
+
+(defn peek-file [file]
+  (with-open [rdr (io/reader file)]
+    (nth (line-seq rdr) 0)))
+
+(defn pipe-delimited? [file]
+  (let [str (peek-file file)]
+    (not (nil? (re-find #"\s+\|\s+" str)))))
+
+(defn comma-delimited? [file]
+  (let [str (peek-file file)]
+    (not (nil? (re-find #",\s+" str)))))
+
+(defn space-delimited? [file]
+  (let [str (peek-file file)]
+    (if (and (not (comma-delimited? file)) (not (pipe-delimited? file)))
+      (not (nil? (re-find #"\s+" str)))
+      false)))
--- test/delimited_file_reader/sniffer_test.clj
+++ test/delimited_file_reader/sniffer_test.clj
@@ -3,7 +3,21 @@
             [delimited-file-reader.sniffer :refer :all]
             [clojure.java.io :as io]))
 
-(def pipe-file (io/file (io/resource "test/pipe.txt")))
-(def comma-file (io/file (io/resource "test/comma.txt")))
-(def space-file (io/file (io/resource "test/space.txt")))
+(def pipe-file (open-file "test/pipe.txt"))
+(def comma-file (open-file "test/comma.txt"))
+(def space-file (open-file "test/space.txt"))
 
+(deftest pipe-test
+  (is (pipe-delimited? pipe-file))
+  (is (not (pipe-delimited? comma-file)))
+  (is (not (pipe-delimited? space-file))))
+
+(deftest comma-test
+  (is (comma-delimited? comma-file))
+  (is (not (comma-delimited? pipe-file)))
+  (is (not (comma-delimited? space-file))))
+
+(deftest space-test
+  (is (space-delimited? space-file))
+  (is (not (space-delimited? pipe-file)))
+  (is (not (space-delimited? comma-file))))
