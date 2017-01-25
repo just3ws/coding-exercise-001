4950ffb89b867856c5c8269d9452c753cb928b32
Mike Hall <mike@just3ws.com>
2017-01-25 13:01:48 -0600

    Load data and verify that the read-in collection is as expected

--- test/delimited_file_reader/reader_test.clj
+++ test/delimited_file_reader/reader_test.clj
@@ -1,10 +1,40 @@
 (ns delimited-file-reader.reader-test
   (:require [clojure.test :refer :all]
             [clojure.pprint :refer :all]
-            [delimited-file-reader.reader :as r]))
+            [clj-time.core :as t]
+            [delimited-file-reader.reader :as r]
+            ))
 
 (def pipe-file (r/open-file "test/pipe.txt"))
 (def comma-file (r/open-file "test/comma.txt"))
 (def space-file (r/open-file "test/space.txt"))
 
-(pprint (r/load-data pipe-file))
+(testing "Read in pipe file and converting to data"
+  (let [data (r/load-data pipe-file)]
+    (is (= 10 (count data)))
+    (testing "verify the first record is as expected"
+      (let [datum (first data)]
+        (is (false? (nil? datum)))
+        (is (= "Green" (get datum :last_name )))
+        (is (= "Abbey" (get datum :first_name )))
+        (is (= "M" (get datum :gender )))
+        (is (= "Orchid" (get datum :favorite_color)))
+        (is (instance? org.joda.time.DateTime (get datum :date_of_birth)))
+        (is (= (t/date-time 2021 10 10) (get datum :date_of_birth))))
+
+
+      )
+
+    (testing "verify the last record is as expected"
+      (let [datum (last data)]
+        (is (false? (nil? datum)))
+        (is (= "Lesch" (get datum :last_name )))
+        (is (= "Emerald" (get datum :first_name )))
+        (is (= "M" (get datum :gender )))
+        (is (= "Tan" (get datum :favorite_color)))
+        (is (instance? org.joda.time.DateTime (get datum :date_of_birth)))
+        (is (= (t/date-time 2021 8 19) (get datum :date_of_birth))))
+
+
+      )
+    ))
