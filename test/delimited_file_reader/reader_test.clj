cbba019ee92a2befed945d452a374eabf4d9c02f
Mike Hall <mike@just3ws.com>
2017-01-25 13:04:03 -0600

    Reading in pipe, comma, and space delimited file. Seems fine

--- test/delimited_file_reader/reader_test.clj
+++ test/delimited_file_reader/reader_test.clj
@@ -20,11 +20,7 @@
         (is (= "M" (get datum :gender )))
         (is (= "Orchid" (get datum :favorite_color)))
         (is (instance? org.joda.time.DateTime (get datum :date_of_birth)))
-        (is (= (t/date-time 2021 10 10) (get datum :date_of_birth))))
-
-
-      )
-
+        (is (= (t/date-time 2021 10 10) (get datum :date_of_birth)))))
     (testing "verify the last record is as expected"
       (let [datum (last data)]
         (is (false? (nil? datum)))
@@ -33,8 +29,48 @@
         (is (= "M" (get datum :gender )))
         (is (= "Tan" (get datum :favorite_color)))
         (is (instance? org.joda.time.DateTime (get datum :date_of_birth)))
-        (is (= (t/date-time 2021 8 19) (get datum :date_of_birth))))
+        (is (= (t/date-time 2021 8 19) (get datum :date_of_birth)))))))
 
+(testing "Read in comma file and converting to data"
+  (let [data (r/load-data comma-file)]
+    (is (= 10 (count data)))
+    (testing "verify the first record is as expected"
+      (let [datum (first data)]
+        (is (false? (nil? datum)))
+        (is (= "Green" (get datum :last_name )))
+        (is (= "Abbey" (get datum :first_name )))
+        (is (= "M" (get datum :gender )))
+        (is (= "Orchid" (get datum :favorite_color)))
+        (is (instance? org.joda.time.DateTime (get datum :date_of_birth)))
+        (is (= (t/date-time 2021 10 10) (get datum :date_of_birth)))))
+    (testing "verify the last record is as expected"
+      (let [datum (last data)]
+        (is (false? (nil? datum)))
+        (is (= "Lesch" (get datum :last_name )))
+        (is (= "Emerald" (get datum :first_name )))
+        (is (= "M" (get datum :gender )))
+        (is (= "Tan" (get datum :favorite_color)))
+        (is (instance? org.joda.time.DateTime (get datum :date_of_birth)))
+        (is (= (t/date-time 2021 8 19) (get datum :date_of_birth)))))))
 
-      )
-    ))
+(testing "Read in space file and converting to data"
+  (let [data (r/load-data space-file)]
+    (is (= 10 (count data)))
+    (testing "verify the first record is as expected"
+      (let [datum (first data)]
+        (is (false? (nil? datum)))
+        (is (= "Green" (get datum :last_name )))
+        (is (= "Abbey" (get datum :first_name )))
+        (is (= "M" (get datum :gender )))
+        (is (= "Orchid" (get datum :favorite_color)))
+        (is (instance? org.joda.time.DateTime (get datum :date_of_birth)))
+        (is (= (t/date-time 2021 10 10) (get datum :date_of_birth)))))
+    (testing "verify the last record is as expected"
+      (let [datum (last data)]
+        (is (false? (nil? datum)))
+        (is (= "Lesch" (get datum :last_name )))
+        (is (= "Emerald" (get datum :first_name )))
+        (is (= "M" (get datum :gender )))
+        (is (= "Tan" (get datum :favorite_color)))
+        (is (instance? org.joda.time.DateTime (get datum :date_of_birth)))
+        (is (= (t/date-time 2021 8 19) (get datum :date_of_birth)))))))
