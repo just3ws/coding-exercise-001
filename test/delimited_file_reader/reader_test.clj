db785a474929a1dc243ae3f5449c039caab2b873
Mike Hall <mike@just3ws.com>
2017-01-25 11:33:53 -0600

    Fixed two styleguide violations

--- test/delimited_file_reader/reader_test.clj
+++ test/delimited_file_reader/reader_test.clj
@@ -5,5 +5,5 @@
 (deftest date-parsing-test
   (is (= "12/19/1975" (r/write-date (r/read-date "12/19/1975"))))
   (is (= "7/6/1975" (r/write-date (r/read-date "7/6/1975"))))
-  (is (= false (r/date? "hello")))
-  (is (= true (r/date? "7/6/1975"))))
+  (is (false? (r/date? "hello")))
+  (is (true? (r/date? "7/6/1975"))))
