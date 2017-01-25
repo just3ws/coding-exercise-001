81eb8b1757ff7b708a4bf6d2c35d273993fa8d22
Mike Hall <mike@just3ws.com>
2017-01-24 22:51:05 -0600

    Can read in test data from resources

--- test/delimited_file_reader/sniffer_test.clj
+++ test/delimited_file_reader/sniffer_test.clj
@@ -1,7 +1,9 @@
 (ns delimited-file-reader.sniffer-test
   (:require [clojure.test :refer :all]
-            [delimited-file-reader.sniffer :refer :all]))
+            [delimited-file-reader.sniffer :refer :all]
+            [clojure.java.io :as io]))
 
-;(def pipe-delimited-file (slurp (.getPath (clojure.java.io/resource "pipe.txt"))))
+(def pipe-file (io/file (io/resource "test/pipe.txt")))
+(def comma-file (io/file (io/resource "test/comma.txt")))
+(def space-file (io/file (io/resource "test/space.txt")))
 
-; (def foo (.getFile (clojure.java.io/resource "pipe.txt")))
