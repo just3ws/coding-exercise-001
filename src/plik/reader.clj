f6188ea0c217d338ac43259859beb6d5815df0be
Mike Hall <mike@just3ws.com>
2017-01-25 18:15:53 -0600

    Minor whitespace

--- src/plik/reader.clj
+++ src/plik/reader.clj
@@ -7,27 +7,33 @@
             [plik.date :as d]
             [plik.sniffer :as sniffer]))
 
-(defn open-file [path] (io/file (io/resource path)))
+(defn open-file
+  [path]
+  (io/file (io/resource path)))
 
-(defn file-exists [path]
-	(.exists (clojure.java.io/as-file path)))
+(defn file-exists?
+  [path]
+  (.exists (clojure.java.io/as-file path)))
 
-(defn transform-header [header]
+(defn transform-header
+  [header]
   (->> header
        s/trim
        ->snake_case_keyword))
 
-(defn cast-date-of-birth [dob]
+(defn cast-date-of-birth
+  [dob]
   (when (d/date? dob) (d/read-date dob)))
 
-(defn load-data [file]
+(defn load-data
+  [file]
   (let [deliminator (sniffer/infer-deliminator file)]
     (with-open [in-file (io/reader file)]
       (->>
-       (csv/read-csv in-file :separator deliminator)
-       (sc/remove-comments)
-       (sc/mappify {:transform-header transform-header :keyify true})
-       (sc/cast-with #(->> % str s/trim))
-       (sc/cast-with {:date_of_birth cast-date-of-birth})
-       doall))))
+        (csv/read-csv in-file :separator deliminator)
+        (sc/remove-comments)
+        (sc/mappify {:transform-header transform-header :keyify true})
+        (sc/cast-with #(->> % str s/trim))
+        (sc/cast-with {:date_of_birth cast-date-of-birth})
+        doall))))
 
