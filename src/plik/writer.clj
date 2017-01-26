16a71aa9650272eb7ad952d23aeb313e6e37cdef
Mike Hall <mike@just3ws.com>
2017-01-26 11:09:44 -0600

    Reuse the internal date format

--- src/plik/writer.clj
+++ src/plik/writer.clj
@@ -1,11 +1,12 @@
 (ns plik.writer
   (:require [clojure.string :as s]
             [clojure.data.json :as json]
+            [plik.date :as d]
             [clj-time.format :as f]))
 
 (defn as-date-string
   [date]
-  (f/unparse (f/formatter "M/d/yyyy") date))
+  (f/unparse d/date-format date))
 
 (defn date-aware-value-writer
   [key value]
