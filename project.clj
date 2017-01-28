3b95500c0875edded6a66b352701ade04565b844
Mike Hall <mike@just3ws.com>
2017-01-28 11:34:36 -0600

    Adding web app to the Clojure project

--- project.clj
+++ project.clj
@@ -5,12 +5,17 @@
             :name "GNU General Public License version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.txt" }
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;; Data App
                  [clj-time "0.13.0"]
                  [org.clojure/data.csv "0.1.3"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/data.json "0.2.6"]
                  [semantic-csv "0.1.1-just3ws"] ;; Because :transform-header
-                 [camel-snake-kebab "0.4.0"]]
+                 [camel-snake-kebab "0.4.0"]
+                 ;; Web App
+                 [compojure "1.5.2"]
+                 [ring/ring-jetty-adapter "1.5.1"]
+                 [environ "1.1.0"]]
   :main ^{ :skip-aot true } plik.runner
   :aot [plik.runner]
   ;: main plik.core
@@ -23,8 +28,11 @@
             [jonase/eastwood "0.2.3"] ;; Clojure linting
             [lein-localrepo "0.5.3"]
             [venantius/ultra "0.5.1"] ;; Improved test output, repl printing
-            [lein-cljfmt "0.5.6"]
-            [lein-pprint "1.1.2"]] ;; Pretty printing
+            [lein-cljfmt "0.5.6"] ;; Linting/style checking
+            [lein-pprint "1.1.2"] ;; Pretty printing
+            [lein-environ "1.1.0"]]
+  :uberjar-name "plik.jar"
+  :profiles { :production {:env {:production true}}}
   :test-selectors {
                    :default (complement :slow)
                    :focus :focus
