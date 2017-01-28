e54ead053cd5f20ad9c42090362effe8855c5c74
Mike Hall <mike@just3ws.com>
2017-01-28 14:01:07 -0600

    Hello, world!

--- src/plik/web/core.clj
+++ src/plik/web/core.clj
@@ -1,1 +1,27 @@
-(ns plik.web.core)
+(ns plik.web.core
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
+
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world!"})
+
+(defroutes app
+  (GET "/" []
+    (splash))
+  (ANY "*" []
+    (route/not-found (slurp (io/resource "404.json")))))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))
+
